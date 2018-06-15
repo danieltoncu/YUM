484f14fa095a753a9b03a672c62b339c6ad3cae3
Daniel Țoncu <toncu.daniel@gmail.com>
2018-06-15 03:53:25 +0300

    ConsoleApp - Environment initialization.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            using (var context = new YUMFoodEntities())
+            {
+            }
         }
     }
 }
