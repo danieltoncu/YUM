3ff800bbec446f60680ffc75fef1bee55c8621ec
Daniel Țoncu <toncu.daniel@gmail.com>
2018-06-17 03:45:29 +0300

    Services - Project structure initialization.

---
+++ Services/YUMServices/Base/BaseService.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.YUMServices.Base
+{
+    abstract class BaseService
+    {
+        protected readonly DbContext _context;
+
+        protected BaseService()
+        {
+            _context = new YUMFoodEntities();
+        }
+    }
+}
