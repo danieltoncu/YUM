82fdb65a01b2afcb73a45a207e36ff1a457f2c3a
Daniel Țoncu <toncu.daniel@gmail.com>
2018-06-17 05:28:09 +0300

    Services - Adding BaseAuthorizedService.

---
+++ Services/YUMServices/Base/BaseAuthorizedService.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Infrastructure.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DataAccess.Infrastructure.Authorization.AuthorizationManager;
+
+namespace Services.YUMServices.Base
+{
+    abstract class BaseAuthorizedService : BaseService
+    {
+        protected readonly AuthorizationManager _authorizationManager;
+        protected readonly int _id;
+
+        protected BaseAuthorizedService(string userName, UserRole userRole) : base()
+        {
+            _authorizationManager = new AuthorizationManager(_context);
+
+            var id = _authorizationManager.AuthorizeMe(userName, userRole);
+
+            _id = id ?? 0;
+        }
+    }
+}
